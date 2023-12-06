a6dd06b65fb4c9d6d721f5f1403b7b251d37b025
shivakumar <shivakumar.yevunja@gmail.com>
2023-12-06 22:31:04 +0530

    Day-3 finding duplicate value

---
+++ src/FindingDuplicate.java
@@ -0,0 +1,17 @@
+public class FindingDuplicate {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,4,3,2};
+        duplicate(arr);
+    }
+    public static void duplicate(int[]arr){
+        int n = arr.length;
+        int[]count = new int[n-1];
+        for(int i=0;i<n;i++){
+            int a = arr[i]-1;
+            count[a]++;
+            if(count[a]>1){
+                System.out.println(arr[i]+" is repeated "+count[a]+" times");
+            }
+        }
+    }
+}
